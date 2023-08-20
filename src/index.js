669f4af249f6d9e29e1a14ef7e6a9fbfcd5a78ee
olekskhom <oleks.khom@gmail.com>
2023-08-20 22:19:22 +0300

    js_pixabay_ref

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import pixabayAPI from './pixabayApi';
 import { createMarkup } from './markupImages';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+// Створюємо екземпляр класу для роботи з Pixabay API
+const pixabayApiInstance = new pixabayAPI(); 
+const ref = {
+  form: document.querySelector('.search-form'), // Знаходимо форму пошуку
+  gallery: document.querySelector('.gallery'), // Знаходимо контейнер для відображення зображень
+  btnLoadMore: document.querySelector('.load-more'), // Знаходимо кнопку "Завантажити ще"
+};
+
+
+// Створюємо об'єкт для SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captions: true,
+  captionDelay: 250,
+});
