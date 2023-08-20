f3e1938b6730d196cd8b24b3957eba0a64ff7135
olekskhom <oleks.khom@gmail.com>
2023-08-20 22:12:19 +0300

    js_1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Notiflix from 'notiflix';
+import pixabayAPI from './pixabayApi';
+import { createMarkup } from './markupImages';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
